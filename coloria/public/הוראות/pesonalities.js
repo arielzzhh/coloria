15c9a2e9ae69f6b6dd4d6e287d2616f1ed83586d
arielzzhh <121157336+arielzzhh@users.noreply.github.com>
2024-05-28 09:46:46 +0300

    Add files via upload

---
+++ coloria/public/הוראות/pesonalities.js
@@ -0,0 +1,71 @@
+const personalityTypes = [
+    {
+        type: "The Nurturer",
+        traits: ["Empathy", "Compassion", "Generosity"]
+    },
+    {
+        type: "The Innovator",
+        traits: ["Creativity", "Curiosity", "Ambition"]
+    },
+    {
+        type: "The Leader",
+        traits: ["Courage", "Confidence", "Integrity"]
+    },
+    {
+        type: "The Idealist",
+        traits: ["Optimism", "Authenticity", "Altruism"]
+    },
+    {
+        type: "The Analyst",
+        traits: ["Intelligence", "Rationality", "Logic"]
+    },
+    {
+        type: "The Adventurer",
+        traits: ["Open-mindedness", "Courage", "Curiosity"]
+    },
+    {
+        type: "The Diplomat",
+        traits: ["Diplomacy", "Patience", "Sensitivity"]
+    },
+    {
+        type: "The Trailblazer",
+        traits: ["Resilience", "Perseverance", "Adaptability"]
+    },
+    {
+        type: "The Visionary",
+        traits: ["Creativity", "Ambition", "Confidence"]
+    },
+    {
+        type: "The Sage",
+        traits: ["Wisdom", "Humility", "Integrity"]
+    }
+];
+
+
+const qualities = [
+  "Empathy",
+  "Compassion",
+  "Creativity",
+  "Intelligence",
+  "Resilience",
+  "Courage",
+  "Generosity",
+  "Honesty",
+  "Loyalty",
+  "Optimism",
+  "Patience",
+  "Ambition",
+  "Humility",
+  "Adaptability",
+  "Assertiveness",
+  "Open-mindedness",
+  "Perseverance",
+  "Self-discipline",
+  "Sensitivity",
+  "Confidence",
+  "Altruism",
+  "Curiosity",
+  "Authenticity",
+  "Resourcefulness",
+  "Tenacity"
+];
